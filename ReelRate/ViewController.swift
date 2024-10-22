693edb2c3a7f7bf6fccad862bd5fb6dfda45aa58
tcorry91 <tcorry@hotmail.com.au>
2024-10-22 19:41:24 +1000

    Initial Commit

---
+++ ReelRate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReelRate
+//
+//  Created by Corry Timothy on 22/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ReelRateTests/ReelRateTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReelRateTests.swift
+//  ReelRateTests
+//
+//  Created by Corry Timothy on 22/10/2024.
+//
+
+import Testing
+@testable import ReelRate
+
+struct ReelRateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
